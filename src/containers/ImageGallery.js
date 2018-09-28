01af5520f35e0fac7df0b61b0a0ca82cab517d36
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-09-28 12:22:44 +0330

    image gallery completed

--- src/containers/ImageGallery.js
+++ src/containers/ImageGallery.js
@@ -3,6 +3,7 @@ import {Image, View, TouchableHighlight} from 'react-native';
 import sizes from '../config/sizes';
 import ImageZoom from 'react-native-image-pan-zoom';
 import { Icon } from 'native-base';
+import ProductImagesList from './ProductImagesList';
 
 class ImageGallery extends Component {
 
@@ -14,7 +15,6 @@ class ImageGallery extends Component {
     }
 
     changeImage = (offsetX) => {
-        console.warn(offsetX);
         if(offsetX > 60) {
             if(this.state.imageNum !== this.props.images.length - 1) {
                 this.setState({imageNum : this.state.imageNum + 1});
@@ -26,21 +26,33 @@ class ImageGallery extends Component {
         }
     }
 
+    setChosenIndex = (index) => {
+        this.setState({imageNum : index});
+    }
+
     render() {
         return(
-            <View style={{flex : 1, backgroundColor:'black'}}>
-                <TouchableHighlight onPress={() => this.props.setModalVisible(false)}>
-                    <Icon type='EvilIcons' name='close' style={{color:'white', fontSize:50}}/>
-                </TouchableHighlight>
-                <ImageZoom cropWidth={sizes.screenWidth}
-                        cropHeight={sizes.screenHeight}
-                        imageWidth={200}
-                        imageHeight={200}
-                        onDragLeft={this.changeImage}
-                        horizontalOuterRangeOffset={(offsetX) => {this.changeImage(offsetX)}}>
-                    <Image style={{width:200, height:200}}
-                        source={{uri:this.props.images[this.state.imageNum]}}/>               
-                </ImageZoom>
+            <View style={{flex : 1, backgroundColor:'black', justifyContent:'space-between'}}>
+                <View style={{justifyContent:'flex-start', alignItems:'flex-start'}}>
+                    <TouchableHighlight onPress={() => this.props.setModalVisible(false)}>
+                        <Icon type='EvilIcons' name='close' style={{color:'white', fontSize:50}}/>
+                    </TouchableHighlight>
+                </View>
+                <View style={{justifyContent:'center', alignItems:'center'}}>
+                    <ImageZoom style={{paddingBottom : 150}}
+                            cropWidth={sizes.screenWidth}
+                            cropHeight={sizes.screenHeight}
+                            imageWidth={200}
+                            imageHeight={120}
+                            onDragLeft={this.changeImage}
+                            horizontalOuterRangeOffset={(offsetX) => {this.changeImage(offsetX)}}>
+                        <Image style={{width:200, height:120}}
+                            source={{uri:this.props.images[this.state.imageNum]}}/>               
+                    </ImageZoom>
+                </View>
+                <View style={{position:'absolute', left:0, right:0, bottom:0}}>
+                    <ProductImagesList images={this.props.images} setChosenIndex={this.setChosenIndex}/>
+                </View>
             </View>
         )
     }
